5352ce5940385b7dda05babfacc82b0a27449af3
Pradeep-SG <pradeepsg612@gmail.com>
2022-03-17 22:09:54 +0530

    Add logo

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ReactComponent as Logo} from "./components/svg_files/logo.svg";
+
 
 ReactDOM.render(
-    <App />,
+  <div>
+    <div className="logo">
+      <Logo />
+    </div>
+    <App />
+  </div>,
   document.getElementById('root')
 );
 
